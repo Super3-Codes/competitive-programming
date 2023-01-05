60d97583d6428773932266b3e8e96f2109445093
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-01-05 17:43:03 +0300

    Create mergeAlternately.py

---
+++ leetcode/mergeAlternately.py
@@ -0,0 +1,7 @@
+  def mergeAlternately(self, word1: str, word2: str) -> str:
+        res=''
+        
+        for i in range(min(len(word1),len(word2))):
+            res += word1[i] + word2[i]
+            
+        return res + word1[i+1:] + word2[i+1:]
