b7f5ff4744f248bcaf69e9278cca8273ceeac4e9
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-01-05 17:25:57 +0300

    Create freqAlphabets.py

---
+++ leetcode/freqAlphabets.py
@@ -0,0 +1,11 @@
+ def freqAlphabets(self, s: str) -> str:
+        ans = ""
+        i = len(s)-1
+        while i>=0:
+            if s[i]=="#":
+                ans = ans + chr(int(s[i-2:i])+96)
+                i=i-2
+            else:
+                ans = ans + chr(int(s[i])+96)
+            i -= 1
+        return ans[::-1]
