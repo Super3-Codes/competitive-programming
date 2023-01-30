2310acaca97d3ce4fbcf089d0d882f36d1d70262
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-01-30 16:37:36 +0300

    Create targetIndices.py

---
+++ leetcode/targetIndices.py
@@ -0,0 +1,7 @@
+ def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == target:
+                result.append(i)
+        return result
