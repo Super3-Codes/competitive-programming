82f3229aa18615e00757726eabe53fb52af9eab9
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-02-11 20:58:04 +0300

    Create twoSum.py

---
+++ leetcode/twoSum.py
@@ -0,0 +1,12 @@
+ def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) -1
+
+        while(right > left):
+            result = numbers[left] + numbers[right]
+            if(result > target):
+                right -= 1
+            elif(result < target):
+                left += 1
+            else:
+                return [left+1,right+1]
