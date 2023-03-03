95a8fcdabff9ea33ed7d0f74590d65065e7a1666
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-03-03 20:09:57 +0300

    Create SelectionSort.py

---
+++ leetcode/SelectionSort.py
@@ -0,0 +1,14 @@
+    def select(self, arr, i):
+        # code here 
+        for j in range(i+1,len(arr)):
+            if arr[j] < arr[i]:
+                i = j
+        return i
+        
+    def selectionSort(self, arr,n):
+    
+        for i in range(n):
+            s = self.select(arr,i)
+            arr[s],arr[i]= arr[i],arr[s]
+        return arr
+   
