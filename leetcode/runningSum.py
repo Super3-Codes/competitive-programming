16cdf950a11e70593674ce5093fc515201fb2a3f
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-03-07 11:57:28 +0300

    Create runningSum.py

---
+++ leetcode/runningSum.py
@@ -0,0 +1,9 @@
+    def runningSum(self, nums: List[int]) -> List[int]:
+        output = []
+
+        for i in range(len(nums)):
+            if i == 0:
+                output.append(nums[i])
+                continue
+            output.append(output[i-1]+nums[i])
+        return output 
