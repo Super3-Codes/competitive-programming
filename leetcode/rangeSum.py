96870142054d4e73ab399135c064d7b4203d6c1c
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-03-07 11:52:15 +0300

    Create rangeSum.py

---
+++ leetcode/rangeSum.py
@@ -0,0 +1,7 @@
+    def __init__(self, nums: List[int]):
+        self.prefixSum = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.prefixSum[i+1] = self.prefixSum[i] + nums[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefixSum[right + 1] - self.prefixSum[left]
