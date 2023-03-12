e086fd1a5d4cbacc11c5a5883eca72f5380c1ba4
Super <79644813+Super3-Codes@users.noreply.github.com>
2023-03-12 12:47:11 +0300

    Create removeDuplicateLetters.py

---
+++ leetcode/removeDuplicateLetters.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+
+        occurence = {}
+        visited = set()
+        stack = []
+
+        for i in range(len(s)):
+            occurence[s[i]] = i
+        
+        for i in range(len(s)):
+
+            if s[i] not in visited:
+                while stack and stack[-1] > s[i] and occurence[stack[-1]] > i:
+                    visited.remove(stack.pop())
+                
+                stack.append(s[i])
+                visited.add(s[i])
+        return ''.join(stack)
